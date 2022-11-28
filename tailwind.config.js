49d5c55854f1feae293e6f5e40f6f8cba5faa039
Hapnees <8dwardm8lton@gmail.com>
2022-11-28 21:09:17 +0300

    Feat (root): begin settings

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			'light-green': '#00FFC6',
+			'dark-blue': '#1E6367',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
