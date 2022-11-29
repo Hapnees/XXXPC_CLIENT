790a55d3f0f994413c057d2fdd36994b2769a37c
Hapnees <8dwardm8lton@gmail.com>
2022-11-29 05:46:18 +0300

    Feat (Menu): create Menu

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { BsFillGearFill } from 'react-icons/bs'
+import cl from './Menu.module.scss'
+
+interface IProps {
+	topIcon: React.ReactNode
+	titles: string[]
+}
+
+const Menu: FC<IProps> = ({ titles, topIcon }) => {
+	return (
+		<div className={cl.wrapper}>
+			{topIcon}
+
+			<ul className={cl.menu}>
+				{titles.map(title => (
+					<li>
+						<BsFillGearFill size={16} className='shrink-0' />
+						<p>{title}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default Menu
