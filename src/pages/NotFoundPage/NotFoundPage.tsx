eca20ddfa3104addc3fb521440b0597946920e09
Hapnees <8dwardm8lton@gmail.com>
2022-11-29 05:46:59 +0300

    Feat (pages): create NotFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+	return (
+		<p className='text-center text-[50px]'>
+			<span>Страница не найдена</span>
+			<span className='text-[#E3ABDC]'> #404</span>
+		</p>
+	)
+}
+
+export default NotFoundPage
