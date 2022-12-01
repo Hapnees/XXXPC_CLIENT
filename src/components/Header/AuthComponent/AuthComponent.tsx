abfdd74aee8d78c1d68bec2e5c5c5f4b74f65883
Hapnees <8dwardm8lton@gmail.com>
2022-12-02 01:09:05 +0300

    Feat (Header): add AuthComponent

---
+++ src/components/Header/AuthComponent/AuthComponent.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import avatar from '../../../assets/img/avatar.jpg'
+import cl from './AuthComponent.module.scss'
+import PopupMenu from './PopupMenu/PopupMenu'
+
+interface IProps {
+	username: string
+}
+
+const AuthComponent: FC<IProps> = ({ username }) => {
+	const [isOpenPopup, setIsOpenPopup] = useState(false)
+
+	return (
+		<div className='relative'>
+			<div
+				className={cl.container__avatar}
+				onClick={() => setIsOpenPopup(!isOpenPopup)}
+				style={
+					isOpenPopup
+						? {
+								borderBottomColor: '#c36eb889',
+								borderBottomLeftRadius: 0,
+								borderBottomRightRadius: 0,
+						  }
+						: {}
+				}
+			>
+				<img src={avatar} className={cl.avatar} alt='' />
+				<p className={cl.title}>{username}</p>
+			</div>
+			<CSSTransition
+				in={isOpenPopup}
+				timeout={300}
+				classNames='popup'
+				unmountOnExit
+			>
+				<PopupMenu />
+			</CSSTransition>
+		</div>
+	)
+}
+
+export default AuthComponent
