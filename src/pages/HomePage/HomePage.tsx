23ffed39ac1cd82e53073487bd652914d43f461c
Hapnees <8dwardm8lton@gmail.com>
2022-12-06 07:25:46 +0300

    Feat (HomePage): add carousel

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import SpecialButton from '../../components/UI/SpecialButton/SpecialButton'
+import React, { useEffect, useState } from 'react'
 import cl from './HomePage.module.scss'
 import image from '../../assets/img/PC.png'
+import { news } from './news'
 
 const HomePage = () => {
+	const [offset, setOffset] = useState(0)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (offset >= (news.length - 1) * 677) {
+				setOffset(0)
+			} else {
+				setOffset(prev => prev + 677)
+			}
+		}, 3_000)
+
+		return () => {
+			clearInterval(interval)
+		}
+	}, [offset])
+
 	return (
 		<div className={cl.wrapper}>
-			<div className='my-auto'>
-				<div className='mb-[40px]'>
-					<p className={cl.title}>КАКАЯ-ТО ВАЖНАЯ ИНФОРМАЦИЯ</p>
-					<p className={cl.body}>
-						Ещё какая-то очень-очень важная инфморация более мелким текстом,
-						которая должна занимать 3 строчки
-					</p>
+			<div className='my-auto w-[677px]'>
+				<div className='overflow-hidden'>
+					<div
+						className='flex duration-300'
+						style={{ transform: `translateX(-${offset}px)` }}
+					>
+						{news.map((el, idx) => (
+							<div className={cl.text__container} key={idx}>
+								<p className={cl.title}>{el.title}</p>
+
+								<p className={cl.body}>{el.body}</p>
+							</div>
+						))}
+					</div>
 				</div>
 				<button className={cl.button}>кнопка</button>
 			</div>
