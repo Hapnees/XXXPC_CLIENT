0f5d0502295bdb217f78ec0f81efc1b28f13feb5
Hapnees <8dwardm8lton@gmail.com>
2022-12-06 07:24:40 +0300

    Feat (pages): edit RepairPage

--- src/pages/RepairPage/RepairPage.tsx
+++ src/pages/RepairPage/RepairPage.tsx
@@ -1,89 +1,32 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React from 'react'
+import RepairCardLaptop from '../../components/RepairCards/RepairCardLaptop/RepairCardLaptop'
+import RepairCardPC from '../../components/RepairCards/RepairCardPC/RepairCardPC'
+import RepairCardPhoneAndroid from '../../components/RepairCards/RepairCardPhoneAndroid/RepairCardPhoneAndroid'
+import RepairCardRestoreData from '../../components/RepairCards/RepairCardRestoreData/RepairCardRestoreData'
 import cl from './RepairPage.module.scss'
-import Menu from '../../components/Menu/Menu'
-import * as titles from './menuTitles/index'
-import * as topIcons from './menuTopIcons/index'
-import { HiArrowSmRight } from 'react-icons/hi'
-import { HiArrowSmLeft } from 'react-icons/hi'
 
 const RepairPage = () => {
-	const [offset, setOffset] = useState(0)
-	const refElement = useRef<HTMLDivElement>(null)
-	const [widthElement, setWidthElement] = useState(0)
-
-	useLayoutEffect(() => {
-		if (refElement.current) setWidthElement(refElement.current.offsetWidth)
-	}, [])
-
-	const onClickLeftArrow = () => {
-		setOffset(prev => {
-			const value = prev + widthElement
-			return value > 0 ? -2 * widthElement : value
-		})
-	}
-
-	const onClickRightArrow = () => {
-		setOffset(prev => {
-			const value = prev - widthElement
-			return Math.abs(value) < widthElement * 3 ? value : 0
-		})
-	}
-
 	return (
-		<div>
+		<div className='flex flex-col items-center'>
 			<p className={cl.top__title}>Ремонт компьютерной техники</p>
-			<div className={cl.carousel}>
-				<div
-					className={cl.container}
-					style={{
-						transform: `translateX(${offset}px)`,
-					}}
-				>
-					<div className={cl.menu__wrapper} ref={refElement}>
-						<Menu topIcon={topIcons.menuPCTop} titles={titles.menuPC} />
-						<Menu
-							topIcon={topIcons.menuSmartPhone}
-							titles={titles.menuSmartPhone}
-						/>
-						<Menu topIcon={topIcons.menuPrinter} titles={titles.menuPrinter} />
-						<Menu topIcon={topIcons.menuTV} titles={titles.menuTV} />
-						<Menu topIcon={topIcons.menuDrive} titles={titles.menuDrive} />
-					</div>
 
-					<div className={cl.menu__wrapper}>
-						<Menu topIcon={topIcons.menuPCTop} titles={titles.menuPC} />
-						<Menu
-							topIcon={topIcons.menuSmartPhone}
-							titles={titles.menuSmartPhone}
-						/>
-						<Menu topIcon={topIcons.menuPrinter} titles={titles.menuPrinter} />
-						<Menu topIcon={topIcons.menuPCTop} titles={titles.menuPC} />
-						<Menu topIcon={topIcons.menuPCTop} titles={titles.menuPC} />
-					</div>
-
-					<div className={cl.menu__wrapper}>
-						<Menu topIcon={topIcons.menuPCTop} titles={titles.menuPC} />
-						<Menu
-							topIcon={topIcons.menuSmartPhone}
-							titles={titles.menuSmartPhone}
-						/>
-						<Menu topIcon={topIcons.menuPrinter} titles={titles.menuPrinter} />
-						<Menu topIcon={topIcons.menuPrinter} titles={titles.menuPrinter} />
-						<Menu topIcon={topIcons.menuPrinter} titles={titles.menuPrinter} />
-					</div>
+			<div className='mt-6'>
+				<div>
+					<ul className={cl.menu}>
+						<li>
+							<RepairCardPC />
+						</li>
+						<li>
+							<RepairCardLaptop />
+						</li>
+						<li>
+							<RepairCardPhoneAndroid />
+						</li>
+						<li>
+							<RepairCardRestoreData />
+						</li>
+					</ul>
 				</div>
-			</div>
-			<div className='flex justify-center gap-6 items-center h-[100px]'>
-				<HiArrowSmLeft
-					size={70}
-					className={cl.arrow}
-					onClick={onClickLeftArrow}
-				/>
-				<HiArrowSmRight
-					size={70}
-					className={cl.arrow}
-					onClick={onClickRightArrow}
-				/>
 			</div>
 		</div>
 	)
