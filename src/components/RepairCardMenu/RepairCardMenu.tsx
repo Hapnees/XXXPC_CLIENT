75a55692905e9cf215b54baa7bdd1415feee3d32
Hapnees <8dwardm8lton@gmail.com>
2022-12-06 06:51:56 +0300

    Feat (components): RepairCardMenu

---
+++ src/components/RepairCardMenu/RepairCardMenu.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import cl from './RepairCardMenu.module.scss'
+import { BsGearFill } from 'react-icons/bs'
+
+interface IProps {
+	title: string
+	array: string[]
+}
+
+const RepairCardMenu: FC<IProps> = ({ title, array }) => {
+	return (
+		<div className='px-4'>
+			<p className='mb-2'>{title}</p>
+
+			<ul className={cl.menu}>
+				{array.map((el, idx) => (
+					<li key={idx}>
+						<BsGearFill />
+						<p>{el}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default RepairCardMenu
