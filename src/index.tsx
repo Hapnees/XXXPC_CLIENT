9e740c9cc8ec2465f15579fa04cc5c9b09ad6b91
Hapnees <8dwardm8lton@gmail.com>
2022-12-06 07:24:55 +0300

    Feat (index.tsx): add toaster

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,16 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
+import { ToastContainer } from 'react-toastify'
+import { toastContainerConfig } from './configs/toast.config'
+import 'react-toastify/dist/ReactToastify.css'
 
 const persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<BrowserRouter>
+		<ToastContainer {...toastContainerConfig} />
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<App />
