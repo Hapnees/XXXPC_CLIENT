7072d287fc774f554329725cbe68f5f3e2fead90
Hapnees <8dwardm8lton@gmail.com>
2022-12-06 07:25:09 +0300

    Feat (App.tsx): add some paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout'
+import MainLayout from './layouts/MainLayout/MainLayout'
 import HomePage from './pages/HomePage/HomePage'
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 import RepairPage from './pages/RepairPage/RepairPage'
+
+const DetailsPage = React.lazy(
+	() =>
+		import(
+			/* webpackChunkName: "DetailsPage" */ './pages/DetailsPage/DetailsPage'
+		)
+)
+
+const ProfilePage = React.lazy(
+	() =>
+		import(
+			/* webpackChunkName: "ProfilePage" */ './pages/ProfilePage/ProfilePage'
+		)
+)
 
 function App() {
 	return (
@@ -11,7 +25,23 @@ function App() {
 			<Route path='/' element={<MainLayout />}>
 				<Route path='' element={<HomePage />} />
 				<Route path='repair' element={<RepairPage />} />
+				<Route
+					path='repair/:slug'
+					element={
+						<Suspense fallback={null}>
+							<DetailsPage />
+						</Suspense>
+					}
+				/>
 				<Route path='*' element={<NotFoundPage />} />
+				<Route
+					path='profile'
+					element={
+						<Suspense fallback={null}>
+							<ProfilePage />
+						</Suspense>
+					}
+				/>
 			</Route>
 		</Routes>
 	)
